4e8b0fe31a6f06ca7eebf2cbf1b9b2ff214e7e3e
Dylan Zocchi <dxocchi@gmail.com>
2018-05-01 20:22:27 -0500

    hlloindt

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -9,7 +9,7 @@ namespace HelloWorld
 			string name = "";
 			int cats = 0;
 			string why = "";
-        	int wish = 0;
+			int wish = 0;
 			Console.WriteLine("Please enter your name: ");
 			name = Console.ReadLine();
 			Console.WriteLine("How many cats do you have: ");
@@ -17,7 +17,7 @@ namespace HelloWorld
 			Console.WriteLine("Why do you have so many cats dude?: ");
 			why = Console.ReadLine();
 			Console.WriteLine("How many cats would you like to get rid of?:");
-        	wish = Convert.ToInt32(Console.ReadLine());
+			wish = Convert.ToInt32(Console.ReadLine());
 			Console.WriteLine("Hello! My name is {0} and I have {1} nasty ass cats. Why do I have these cats? {2}. I wish I had {3} cats!", name, cats, why, cats - wish);
 		}
     }
