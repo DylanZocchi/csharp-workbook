139d00de2c6002061aba4cf7807e55a96424ed76
Dylan Zocchi <dxocchi@gmail.com>
2018-05-01 20:21:19 -0500

    hlloindent

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -16,7 +16,7 @@ namespace HelloWorld
 			cats = Convert.ToInt32(Console.ReadLine());
 			Console.WriteLine("Why do you have so many cats dude?: ");
 			why = Console.ReadLine();
-        	Console.WriteLine("How many cats would you like to get rid of?:");
+			Console.WriteLine("How many cats would you like to get rid of?:");
         	wish = Convert.ToInt32(Console.ReadLine());
 			Console.WriteLine("Hello! My name is {0} and I have {1} nasty ass cats. Why do I have these cats? {2}. I wish I had {3} cats!", name, cats, why, cats - wish);
 		}
